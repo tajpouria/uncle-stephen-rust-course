9f0f101b8622fa42594d942eeef5cb14d5d66644
tajpouria <46822325+tajpouria@users.noreply.github.com>
2026-05-05 21:52:47 +0200

    started

--- deck/src/main.rs
+++ deck/src/main.rs
@@ -1,3 +1,9 @@
+#[derive(Debug)]
+struct Deck {
+    decks: Vec<String>,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let deck = Deck { decks: vec![] };
+    println!("decks: {:?}", deck.decks);
 }
